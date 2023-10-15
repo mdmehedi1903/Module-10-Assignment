ea969ef5845943e9f4995f084c00cff0b6de88dc
Your Name <mdmehedi1903@gmail.com>
2023-10-15 07:20:17 +0600

    OTP Updated

--- src/models/OTPModel.js
+++ src/models/OTPModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const otpSchema = new mongoose.Schema({
   email: { type: String},
   otp: { type: String},
-  status: { type: String, default: 0},
+  status: { type: Number, default: 0},
 },
     {
         versionKey: false,
